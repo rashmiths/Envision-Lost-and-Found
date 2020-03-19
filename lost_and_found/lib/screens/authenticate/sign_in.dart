d6b8cd2b42c3e1dc2c75da325d920dbfd08304ef
Sharvani2002 <sharvanilaxmisomayaji@gmail.com>
2020-03-19 17:44:20 +0530

    Changes done by Rashmith

--- lost_and_found/lib/screens/authenticate/sign_in.dart
+++ lost_and_found/lib/screens/authenticate/sign_in.dart
@@ -31,78 +31,80 @@ class _SignInState extends State<SignIn>
     return loading? Loading() : Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.pink,
         elevation: 0.0,
         title: Text('Sign in to Lost and Found'),
         actions: <Widget>[
           FlatButton.icon(
-           onPressed: (){
+            onPressed: (){
               widget.toggleView();
-           },
-           icon: Icon(Icons.person),
-           label: Text('Register'),
-           )
+            },
+            icon: Icon(Icons.person),
+            label: Text('Register'),
+          )
         ],
-        ),
+      ),
       body:  Stack(
           fit: StackFit.expand,
           children: <Widget>[
-             Image(image:  AssetImage("assets/1.jpeg"),
+            Image(image:  AssetImage("assets/1.jpeg"),
               fit: BoxFit.cover,
               color: Colors.black54,
               colorBlendMode:BlendMode.darken,
             ),
-             Image(image:  AssetImage("assets/nitk1.jpeg",),
 
-            ),
-             Column(
+            Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                 FlutterLogo(
-                  size:0
+                new CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  radius: 100.0,
+                  child: Image.asset('assets/nitk1.jpeg'),
                 ),
                 Form(
-                  key: _formKey,
-                  child : Theme(
-                    data: ThemeData(
-                    brightness: Brightness.dark,primarySwatch: Colors.teal,
-                      inputDecorationTheme: InputDecorationTheme(
-                        labelStyle: TextStyle(
-                          color:Colors.teal,
-                        )
-                      )
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                         TextFormField( 
-                          validator: (val) => val.isEmpty ? 'Enter an email': null,
-                          onChanged: (val){
-                            setState(() => email= val);
+                    key: _formKey,
+                    child : Theme(
+                      data: ThemeData(
+                          brightness: Brightness.dark,primarySwatch: Colors.teal,
+                          inputDecorationTheme: InputDecorationTheme(
+                              labelStyle: TextStyle(
+                                color:Colors.pink,
+                              )
+                          )
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          TextFormField(
+                            validator: (val) => val.isEmpty ? 'Enter an email': null,
+                            onChanged: (val){
+                              setState(() => email= val);
 
-                          },
-                          decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                          keyboardType: TextInputType.text,
-                        ),
-                         TextFormField(
-                          validator: (val) => val.length<6 ? 'Password should be atleast 6 characters long': null,
-                           onChanged: (val){
-                             setState(() => password= val);
+                            },
+                            decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                            keyboardType: TextInputType.text,
+                          ),
+                          SizedBox(height:5.0 ,),
 
-                                      },
-                          decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                        ),
-                        
-                        //Though email id and password are taken here, for now anonymous method is tried to be
-                        //taken here(Change this later)
+                          TextFormField(
+                            validator: (val) => val.length<6 ? 'Password should be atleast 6 characters long': null,
+                            onChanged: (val){
+                              setState(() => password= val);
 
-                         //if there is an error later, try to change RaisedButton to MaterialButton and add height
-                         RaisedButton(
-                          color:Colors.teal,
-                          textColor: Colors.white,
-                          child:  Text("login"),
+                            },
+                            decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(height:5.0 ,),
+                          //Though email id and password are taken here, for now anonymous method is tried to be
+                          //taken here(Change this later)
+
+                          //if there is an error later, try to change RaisedButton to MaterialButton and add height
+                          RaisedButton(
+                            color:Colors.teal,
+                            textColor: Colors.white,
+                            child:  Text("login"),
                             onPressed: () async {
                               if(_formKey.currentState.validate()){
                                 setState(()=> loading= true);
@@ -118,17 +120,17 @@ class _SignInState extends State<SignIn>
                                 print(password);
 
                               }
-                          },
-                          splashColor: Colors.redAccent,
-                        ),
-                        SizedBox(height: 12.0),
-                        Text(
-                          error,
-                          style: TextStyle(color: Colors.red, fontSize: 14.0),
-                        )
-                      ],
-                    ),
-                  )
+                            },
+                            splashColor: Colors.redAccent,
+                          ),
+                          SizedBox(height: 12.0),
+                          Text(
+                            error,
+                            style: TextStyle(color: Colors.red, fontSize: 14.0),
+                          )
+                        ],
+                      ),
+                    )
 
                 )
               ],
--- lost_and_found/lib/screens/home/home.dart
+++ lost_and_found/lib/screens/home/home.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:lost_and_found/models/lostUser.dart';
-import 'package:lost_and_found/screens/home/settings_form.dart';
+import 'package:lost_and_found/screens/home/PhotoUpload.dart';
 import 'package:lost_and_found/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'package:lost_and_found/services/database.dart';
@@ -14,21 +14,14 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    void _showSettingsPanel(){
-      showModalBottomSheet(context: context, builder: (context){
-        return Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-          child: SettingsForm(),
-        );
-      });
-    }
+
     return StreamProvider<List<LostUser>>.value(
       value: DatabaseService().lostUsers,
       child: Scaffold(
-      backgroundColor: Colors.purple[200],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Lost and Found'),
-        backgroundColor: Colors.black26,
+        backgroundColor: Colors.pink,
         elevation: 0.0,
         actions: <Widget>[
           FlatButton.icon(
@@ -40,14 +33,22 @@ class Home extends StatelessWidget {
             },
           ),
         FlatButton.icon(
-          onPressed: ()=> _showSettingsPanel(),
-           icon: Icon(Icons.settings),
-            label: Text('settings'),
+          onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context)
+                {
+                  return new UploadPhotoPage();
+                })
+            );
+          },
+           icon: Icon(Icons.add_a_photo),
+            label: Text('add a photo'),
             )
         ],
       ),
-      body: LostUserList(),
+      body: DefaultTabController(length: 1, child:TabBarView(children: <Widget>[FlatButton.icon(onPressed: null, icon: Icon(Icons.add), label: Text("lost photos"))],),
       ),
-    );
+    ));
   }
 }
